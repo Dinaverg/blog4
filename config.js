b8c96240048450bcd6091199cfaa78887f0842da
Darryl Williams <kirbydinaverg@gmail.com>
2019-02-10 02:47:44 -0600

    build

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://Dina:thinkful1@ds213705.mlab.com:13705/blog';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app';
 exports.PORT = process.env.PORT || 8080;
